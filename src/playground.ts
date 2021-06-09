295a8c13737ba0e43210cae387f546ff72e37e75
Luis del Amo <luis@salonkee.lu>
2021-06-10 00:15:00 +0200

    type function getNumExclamations

--- src/playground.ts
+++ src/playground.ts
@@ -1,4 +1,17 @@
+/* 
 const shout = (text: string, upperCase: boolean) =>
   (upperCase ? text.toUpperCase() : text) + "!!!!";
 
 console.log(shout("hola", true));
+*/
+
+//Typyng functions
+const shout = (text: string, getNumExclamations: () => number): string =>
+  text.toUpperCase() + "!".repeat(getNumExclamations());
+
+const getRandom = (): number => Math.ceil(Math.random() * 10);
+
+console.log(shout("hola", getRandom));
+console.log(shout("hola", getRandom));
+console.log(shout("hola", getRandom));
+console.log(shout("hola", getRandom));

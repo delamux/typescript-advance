889d8c94ff674f75465695488209f0b73eb5ff69
Luis del Amo <luis@salonkee.lu>
2021-06-10 00:58:50 +0200

    generics in interfaces

--- src/playground.ts
+++ src/playground.ts
@@ -1,19 +1,11 @@
 //  ************* GENERICS *************
 
-/* function first(list: number[]): number {
-  return list[0];
+interface State<T> {
+  value: T;
 }
 
-first[("hello", "world")]; */
+const stringState: State<string> = {
+  value: "hello",
+};
 
-// To no use any we use the generics wich is resolved at the moment to call the method
-
-function first<T>(list: T[]): T {
-  return list[0];
-}
-
-const firstArrow = <T>(list: T[]): T => list[0];
-
-// first(["hello", "world", 9]);
-
-const result = first<string | number>(["hello", "world", 9]);
+const result = stringState.value;
